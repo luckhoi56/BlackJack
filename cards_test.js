3c527906b8fddf9da7f35d77666d50456a468834
exe714 <milesselim4@gmail.com>
2020-11-19 21:53:20 -0600

    works with no extra players

--- cards_test.js
+++ cards_test.js
@@ -1,104 +1,141 @@
-
+    var deckID;
     //const fetch = require("node-fetch");
+    var stay= false;
+    var done =false;
     var players = 
                 [
-                    {"count":0,"hands":[]},
-                    {"count":0,"hands":[]}
-
-]
+                    {"value":0,"hands":[]},
+                    {"value":0,"hands":[]}
+                ]
                 
     
     
     
-    function fetchDeck()
-    {    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6").then(response => response.json()).then(data=>
-    {
-        
-        deckID = data.deck_id
-        console.log("Deck id " + deckID)
-        
-        // console.log("cards  " + cards)
-        m_cards =drawCard(deckID,players.length *2)
-        
-    })
-    }
-    
-    
-    function drawCard(deckID, count){
-        var m_cards = []
-        var card_count = count
-        fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${card_count}`)
-            .then(reponse=>reponse.json()).then(data=>{
-                
-                data.cards.forEach(card=>{
-                    console.log(card)
-                    m_cards.push(card)
-                    /*
-                    et i = 0; i < data.length; i++){
-                    console.log(data.cards[i])
-                    
-                    */
-                });
-                distribute(m_cards)
-                
-            }
-        )
-        
-    }
-
-    function distribute(cards){
-        for(i =0; i<players.length;i++)
+    async function runGame()
+    {   
+        let response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6")
+        let data = await response.json();
+        deckID = data.deck_id;
+        for(i=0;i<players.length;i++)
         {
-            players[i].hands.push(cards.pop());
+            console.log(i);
+            hit(players[i]);
+            
+            hit(players[i]);
+            
+        }
         
-            players[i].hands.push(cards.pop());}
-        // players[0].count = 2
-        // players[0].hands.push(cards[0])
-        // players[0].hands.push(cards[1])
+        // let drawResponse = await fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${players.length*2}`);
+        // let cardData = await drawResponse.json();
+        // p_cards = cardData.cards;
+        // distribute(p_cards)
+        render();
+        while(!done){
+            max= Math.max.apply(Math,players.map(function(player){return player.value}))
+            console.log(max);
+            done  = (players[0].value>=21||stay)&&(max>=21||players[1].value>=17);
+            console.log('me: '+players[0].value);
+            console.log('dealer: '+players[1].value);
+            
+            if(stay && players[1].value<17)
+            {
+                hit(players[1]);
+            }
+            // players.forEach(player=>console.log(player.value));
+            render();
+            
+            await new Promise(r => setTimeout(r, 2000));
+            
+            if(done)
+            {
 
-        // players[1].count = 2
-        // players[1].hands.push(cards[2])
-        // players[1].hands.push(cards[3])
-        // console.log(players[0].hands[0].value)
-        // console.log(players[1].hands[0].value)
-         render(players)
+                if(players[1].value>21) alert('You Win')
+                
+                else if(players[0]==max) alert('You Win');
+                else alert('You Lose');
+            }
+        }
+        
+    }
+    
+    
+    async function hit(player){
+        
+        
+        let response = await fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+        let cardData = await response.json();
+        // console.log(cardData.cards[0])
+        let cardValue = await cardData.cards[0].value==='ACE'?1:(cardData.cards[0].value==='KING'||cardData.cards[0].value==='QUEEN'||cardData.cards[0].value==='JACK')? 10:parseInt(cardData.cards[0].value);
+        console.log(cardValue);
+        player.value = player.value==10&& cardValue==1? 21: parseInt(player.value)+ await cardValue;
+        await player.hands.push( await cardData.cards[0]);
+        
     }
 
-    function render(players){
+    // function distribute(cards){
+    //     console.log('distributing cards');
+    //     for(i =0; i<players.length;i++)
+    //     {
+    //         players[i].hands.push(cards.pop());
+        
+    //         players[i].hands.push(cards.pop());}
+    //     // players[0].count = 2
+    //     // players[0].hands.push(cards[0])
+    //     // players[0].hands.push(cards[1])
+
+    //     // players[1].count = 2
+    //     // players[1].hands.push(cards[2])
+    //     // players[1].hands.push(cards[3])
+    //     // console.log(players[0].hands[0].value)
+    //      console.log(players[1].hands[0].value)
+    //     //  render(players)
+    //     console.log('done distributing');
+    // }
+
+    function render(){
         var user = document.getElementById("user")
         var dealer = document.getElementById("dealer")
         var player= document.getElementById('player');
+        user.innerHTML ='<div class="col">User</div>';
+        dealer.innerHTML ='<div class="col">Dealer</div>';
+        player.innerHTML ='<div class="col">Other player</div>';
+        console.log('going to render')
         for(i=0;i<players.length;i++)
         {
             if(i==0)
             {
-                for(let k=1;k<players[0].hands.length;k++)
-                {
-                    faceUpD = document.createElement("img")
-                    faceUpD.src = players[0].hands[k].images.png
-                    dealer.appendChild(faceUpD)  
+               
+                for(let j=0;j<players[0].hands.length;j++){
+                    let faceUpP = document.createElement("img")
+                    faceUpP.src = players[0].hands[j].images.png
+                    user.appendChild(faceUpP)  
                 }
-        
             }
             else if(i==1)
             {
-               
-                for(let j=0;j<players[1].hands.length;j++){
-                faceUpP = document.createElement("img")
-                faceUpP.src = players[1].hands[j].images.png
-                user.appendChild(faceUpP)  
-            }}
+                for(let k=1;k<players[1].hands.length;k++)
+                {
+                    let faceUpD = document.createElement("img")
+                    faceUpD.src = players[1].hands[k].images.png
+                    dealer.appendChild(faceUpD)  
+                }
+            }
             else{
                 
                 for(let k=1;k<players[i].hands.length;k++)
                 {
-                    faceUpD = document.createElement("img")
+                    let faceUpD = document.createElement("img")
                     faceUpD.src = players[i].hands[k].images.png
                     player.appendChild(faceUpD)  
                 }
             }
         }
        
+    }
+    function toStay()
+    {
+        stay=true;
+        console.log('stay');
     }
 
 //fetchDeck()
